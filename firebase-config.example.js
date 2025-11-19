daab6b07d86a1e9dc69a1da8b61e1c2b3744cddc
Bruno <bmoreiraribeiro@gmail.com>
2025-11-19 03:06:18 +0000

    feat: adicionar Firebase sync para tarefas PESTA e progresso de estudos

---
+++ firebase-config.example.js
@@ -0,0 +1,18 @@
+// Firebase Configuration Template
+// Copie este ficheiro para "firebase-config.js" e adicione as suas credenciais
+
+// INSTRUÇÕES:
+// 1. Aceda a https://console.firebase.google.com/
+// 2. Crie um novo projeto
+// 3. Adicione uma Web App
+// 4. Copie as credenciais abaixo
+// 5. Renomeie este ficheiro para "firebase-config.js"
+
+const firebaseConfig = {
+    apiKey: "SUA_API_KEY_AQUI",
+    authDomain: "seu-projeto.firebaseapp.com",
+    projectId: "seu-projeto-id",
+    storageBucket: "seu-projeto.appspot.com",
+    messagingSenderId: "123456789",
+    appId: "1:123456789:web:abc123"
+};
---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase Configuration
+// Este ficheiro contém as credenciais do Firebase
+// IMPORTANTE: Adicione este ficheiro ao .gitignore se quiser mantê-lo privado
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBLcU-3JPu3NYNlJgvWg9Hr4cz6DanXeU0",
+    authDomain: "pesta-organizador.firebaseapp.com",
+    projectId: "pesta-organizador",
+    storageBucket: "pesta-organizador.firebasestorage.app",
+    messagingSenderId: "1026990979564",
+    appId: "1:1026990979564:web:3b6a7c89dd9c492369e8c9"
+};
